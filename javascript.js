d094de7c1ec374bcff08a18c7c8abbc69e31aa4c
martin madisson <Martin.Madisson@tpt.lan>
2026-03-26 14:29:21 +0200

    esimene commit

---
+++ javascript.js
@@ -0,0 +1,47 @@
+const eesnimi = document.querySelector('#nimi');
+const Sisesta = document.querySelector('#salvesta');
+
+let eesnimi = "";
+
+
+Sisesta.addEventListener('click', function() {
+    eesnimi = nimi.value;
+    save(eesnimi);
+    console.log(sisesta);
+
+});
+
+async function save(name, place) {
+    const response = await fetch('tinkr.tech/sdb/Martin/mang', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: nimi.value})
+    });
+}
+console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
