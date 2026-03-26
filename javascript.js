1e582c73e2c8f81d32b0154e604479803ca3da9d
Martin Madisson <martinmadisson2008@gmail.com>
2026-03-26 20:02:56 +0200

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,39 @@
+let nimi = document.querySelector('#nimi');
+const Sisesta = document.querySelector('#salvesta');
+
+let eesnimi = "";
+
+Sisesta.addEventListener('click', async function () { 
+    eesnimi = nimi.value; 
+    console.log(eesnimi); 
+});
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
