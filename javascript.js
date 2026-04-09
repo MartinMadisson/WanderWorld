2b19a9c088b0cf8131b99f47a7f1271d2be74af3
martin madisson <Martin.Madisson@tpt.lan>
2026-04-09 13:07:49 +0300

    wanderworld

--- javascript.js
+++ javascript.js
@@ -1,46 +1,35 @@
-const eesnimi = document.querySelector('#nimi');
-const Sisesta = document.querySelector('#salvesta');
+let nimi = document.querySelector('#nimi');
+const Liitu = document.querySelector('#salvesta');
+const kaart = document.querySelector('#kaart');
 
-let eesnimi = "";
+Liitu.addEventListener('click', async () => {
+    let eesnimi = nimi.value;
+    console.log(eesnimi);
 
-Sisesta.addEventListener('click', function() {
-    eesnimi = nimi.value;
-    save(eesnimi);
-    console.log(sisesta);
-
-});
-
-async function save(name, place) {
-    const response = await fetch('tinkr.tech/sdb/Martin/mang', {
+    const vastus = await fetch('https://tinkr.tech/sdb/Martin/wanderworld', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name: nimi.value})
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({action: 'join', username: eesnimi })
     });
-}
-console.log(data);
 
+    const andmed = await vastus.json();
+    console.log(andmed);
 
+    localStorage.setItem('playerkey', andmed.player_key);
+    localStorage.setItem('username', eesnimi);
+});
+setInterval(async function() {
+    const vastus = await fetch('https://tinkr.tech/sdb/Martin/wanderworld', {
+        method: 'GET'
+    });
+    const olek = await vastus.json();
+    console.log(olek);
 
+    kaart.innerHTML = '';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for (const mangja of olek.players) {
+        const div = document.createElement('img');  
+        div.style.position = 'absolute';
+        div.style.left = mangja.x + 'px';
+        div.style.top = mangja.y + 'px';
+    }     kaart.appendChild(div);
