d8ac8d4706c477207979d762dca67c3d04db5291
Martin Madisson <martinmadisson2008@gmail.com>
2026-04-10 16:21:03 +0300

    WanderWorld update

--- javascript.js
+++ javascript.js
@@ -2,34 +2,64 @@ let nimi = document.querySelector('#nimi');
 const Liitu = document.querySelector('#salvesta');
 const kaart = document.querySelector('#kaart');
 
-Liitu.addEventListener('click', async () => {
+
+player_key = localStorage.getItem('player_key');
+
+Liitu.addEventListener('click', async function () {
     let eesnimi = nimi.value;
     console.log(eesnimi);
 
     const vastus = await fetch('https://tinkr.tech/sdb/Martin/wanderworld', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({action: 'join', username: eesnimi })
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ action: 'join', username: eesnimi })
     });
 
     const andmed = await vastus.json();
     console.log(andmed);
 
-    localStorage.setItem('playerkey', andmed.player_key);
-    localStorage.setItem('username', eesnimi);
+    localStorage.setItem('player_key', andmed.player_key);
+    player_key = andmed.player_key; 
 });
-setInterval(async function() {
-    const vastus = await fetch('https://tinkr.tech/sdb/Martin/wanderworld', {
-        method: 'GET'
-    });
+
+setInterval(async function () {
+    const vastus = await fetch('https://tinkr.tech/sdb/Martin/wanderworld');
     const olek = await vastus.json();
-    console.log(olek);
 
     kaart.innerHTML = '';
 
-    for (const mangja of olek.players) {
-        const div = document.createElement('img');  
+    for (const mangija of olek.players) {
+        const div = document.createElement('div');
         div.style.position = 'absolute';
-        div.style.left = mangja.x + 'px';
-        div.style.top = mangja.y + 'px';
-    }     kaart.appendChild(div);
+        div.style.left = mangija.x + 'px';
+        div.style.top = mangija.y + 'px';
+
+        const pilt = document.createElement('img');
+        pilt.src = 'https://tinkr.tech' + mangija.image;
+        pilt.width = 32;
+        pilt.height = 32;
+        pilt.style.imageRendering = 'pixelated';
+
+        const name = document.createElement('p');
+        name.textContent = mangija.username;
+
+        div.appendChild(pilt);
+        div.appendChild(name);
+        kaart.appendChild(div);
+    }
+}, 1000);
+
+kaart.addEventListener('click', async function (e) {
+    let x = e.offsetX;
+    let y = e.offsetY;
+    const player_key = localStorage.getItem('player_key');
+
+    const vastus = await fetch('https://tinkr.tech/sdb/Martin/wanderworld', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ action: 'move', player_key: player_key, x: x, y: y })
+    });
+
+    const andmed = await vastus.json();
+    console.log(andmed);
+});
